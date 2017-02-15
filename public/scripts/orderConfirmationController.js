0132f8043dc51022c310609b33e5c244eadc5dd9
Adam Brodziak <abrodziak@future-processing.com>
2017-02-15 14:21:50 +0100

    Fetch order confirmation from server

--- public/scripts/orderConfirmationController.js
+++ public/scripts/orderConfirmationController.js
@@ -1,3 +1,6 @@
-angular.module('app').controller('orderConfirmationController', function () {
-    
+angular.module('app').controller('orderConfirmationController', function ($scope, $routeParams, Order) {
+    var orderId = $routeParams.orderId;
+
+    $scope.order = Order.get({get: orderId});
+    console.log($scope.order);
 });

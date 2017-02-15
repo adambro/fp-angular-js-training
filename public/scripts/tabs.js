8e9cc03d4fe424ccda4c22dd82cddc856ccdb47e
Adam Brodziak <abrodziak@future-processing.com>
2017-02-15 11:13:29 +0100

    WIP Tabset implementation that does not work!

--- public/scripts/tabs.js
+++ public/scripts/tabs.js
@@ -1,5 +1,13 @@
 angular.module("app").directive("tab", function() {
-
+    return {
+        // specify that directive needs parent
+        // ^ - find in any parent
+        require: "^tabset",
+        // controller is always 4th param in link function
+        link: function($scope, $elem, $attr, tabsetContoller) {
+            tabsetContoller.registerTab($attr.title);
+        }
+    }
 });
 
 angular.module("app").directive("tabset", function() {
@@ -7,9 +15,17 @@ angular.module("app").directive("tabset", function() {
         template: '<div class="tabs">' +
             '<ul>' +
             '<li ng-repeat="tab in knownTabs">{{tab}}</li>' +
-            '</ul></div>',
+            '</ul>' +
+            '</div>',
         link: function($scope) {
             $scope.knownTabs = ['A', 'B', 'C'];
+        },
+        // metody dostępne dla dyrektyw potomnych
+        controller: function($scope) {
+            $scope.knownTabs = [];
+            this.registerTab = function(tabTitle) {
+                $scope.knownTabs.push(tabTitle);
+            }
         }
     };
 });

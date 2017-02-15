a8d46692941d4861a027cac3948ffee1ce988024
Adam Brodziak <abrodziak@future-processing.com>
2017-02-15 11:31:15 +0100

    Tabs switching

--- public/scripts/tabs.js
+++ public/scripts/tabs.js
@@ -6,6 +6,15 @@ angular.module("app").directive("tab", function() {
         // controller is always 4th param in link function
         link: function($scope, $elem, $attr, tabsetContoller) {
             tabsetContoller.registerTab($attr.title);
+
+            $scope.$on('tabSelected', function(ev, tabTitle) {
+                if (tabTitle == $attr.title) {
+                    $elem.show();
+                }
+                else {
+                    $elem.hide();
+                }
+            });
         }
     }
 });
@@ -14,15 +23,18 @@ angular.module("app").directive("tabset", function() {
     return {
         template: '<div class="tabs">' +
             '<ul>' +
-            '<li ng-repeat="tab in knownTabs">{{tab}}</li>' +
+            '<li ng-repeat="tab in knownTabs" ng-click="showTab(tab)">{{tab}}</li>' +
             '</ul>' +
             '<div ng-transclude></div>' +
             '</div>',
         transclude: true,
-        // link function would replace the values, because it's executed at the end
-        // link: function($scope) {
-        //     $scope.knownTabs = ['A', 'B', 'C'];
-        // },
+        link: function($scope) {
+            $scope.showTab = function(tabTitle) {
+                $scope.$broadcast('tabSelected', tabTitle);
+            }
+
+            $scope.showTab($scope.knownTabs[0]);
+        },
         // add local scope
         scope: true,
         // metody dostępne dla dyrektyw potomnych
